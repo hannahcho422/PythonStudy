a28cb589722dae3c9764de4e6d327cf7f00b8697
Hannah Cho <hannahcho422@gmail.com>
2026-02-15 16:52:14 +0900

    init: Initial commit

---
+++ Basic/DataType/string_test.py
@@ -0,0 +1,22 @@
+a = 'python1'
+b = "python2"
+c = "작은 따옴표는 ' 모양입니다."
+d = '큰 따옴표는 " 모양입니다.'
+
+print(a)
+print(type(a))
+print()
+
+print(b)
+print(type(b))
+print()
+
+print(c)
+print(type(c))
+print()
+
+print(d)
+print(type(d))
+
+
+
