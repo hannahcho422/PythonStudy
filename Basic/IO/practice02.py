974cb7f8f4636edc63b46ea7f7ff5c24afa59d4c
Hannah Cho <hannahcho422@gmail.com>
2026-02-16 15:20:45 +0900

    init: Initial commit

---
+++ Basic/IO/practice02.py
@@ -0,0 +1,4 @@
+participant = int(input("Enter the number of participants: "))
+print("Number of chicken neeed: ", participant * 1)
+print("Number of beer neeed: ", participant * 2)
+print("Number of cake neeed: ", participant * 4)
