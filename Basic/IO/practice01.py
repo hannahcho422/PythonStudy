4c741cd4370b05b598ef8d7d327e16c484d9b258
Hannah Cho <hannahcho422@gmail.com>
2026-02-16 15:17:22 +0900

    init: Initial commit

---
+++ Basic/IO/practice01.py
@@ -0,0 +1,7 @@
+name = input("이름 입력: ")
+print(name, " 씨, 안녕하세요?")
+
+int1 = int(input("Enter the first integer: "))
+int2 = int(input("Enter the second integer: "))
+sum = int1 + int2
+print(int1, " + ", int2, " = ", sum)
