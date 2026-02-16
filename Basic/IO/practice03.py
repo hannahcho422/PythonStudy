8858636a0af84737eff4e16818861a2c5e183b71
Hannah Cho <hannahcho422@gmail.com>
2026-02-16 15:23:30 +0900

    init: Initial commit

---
+++ Basic/IO/practice03.py
@@ -0,0 +1,10 @@
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+
+print("Before swapping: x = ", x, " y = ", y)
+
+temp = x
+x = y
+y = temp
+
+print("After swapping: x = ", x, " y = ", y)
