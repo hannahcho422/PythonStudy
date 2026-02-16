6122ea6b524dc04c3faed8663a9247fead9a6547
Hannah Cho <hannahcho422@gmail.com>
2026-02-16 16:46:24 +0900

    init: Initial commit:

---
+++ Basic/Conditions/practice04.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter number1: "))
+num2 = int(input("Enter number2: "))
+
+if num1 > num2 :
+    print("Bigger number: ", num1)
+else :
+    print("Bigger number: ", num2)
+
+'''
+num1 = int(input("Enter number1: "))
+num2 = int(input("Enter number2: "))
+
+if (num1 > num2) :
+    max = num1
+else :
+    max = num2
+
+print("Bigger number: ", max)
+'''
