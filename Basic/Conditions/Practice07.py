0b510e03e4bd56e6d66f7abe8cfd4146557174ab
Hannah Cho <hannahcho422@gmail.com>
2026-02-21 14:48:07 +0900

    init: Initial commit

---
+++ Basic/Conditions/Practice07.py
@@ -0,0 +1,8 @@
+number = float(input("Enter a number: "))
+
+if number > 0 :
+    print("Positive number")
+elif number == 0 :
+    print("0")
+elif number < 0 :   # else :
+    print("Negative number")
