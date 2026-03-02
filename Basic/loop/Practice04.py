8cd3e1d9a35d4d32dfdc0d5c018f9523209778a7
Hannah Cho <hannahcho422@gmail.com>
2026-03-03 01:33:54 +0900

    init: Initial commit

---
+++ Basic/loop/Practice04.py
@@ -0,0 +1,12 @@
+print("종료하려면 음수를 입력하시오")
+grade = int(input("성적을 입력하시오: "))
+
+sum = 0
+count = 0
+
+while grade > 0 :
+    sum  = sum + grade
+    count += 1
+    grade = int(input("성적을 입력하시오: "))
+    
+print("성적의 평균은 ", sum / count)
