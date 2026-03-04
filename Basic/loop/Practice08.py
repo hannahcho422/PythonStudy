cd985657d2d71aa13cca0a70a9220c811fe383d7
Hannah Cho <hannahcho422@gmail.com>
2026-03-04 11:03:03 +0900

    init: Initial commit

---
+++ Basic/loop/Practice08.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first integer: "))
+
+while num1 != 0 :
+    num2 = int(input("Enter the second integer: "))
+    print(num1, " + " , num2, " = ", num1 + num2)
+    num1 = int(input("Enter the first integer: "))
+
+print("0 entered. Break from loop.")
