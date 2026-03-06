e86616562567938e16ef9a8f290f4c29d8f25ff9
Hannah Cho <hannahcho422@gmail.com>
2026-03-06 18:55:32 +0900

    init: Initial commit

---
+++ Basic/List/Practice02.py
@@ -0,0 +1,10 @@
+# Combine lists
+
+lang1 = ['C', 'C++', 'Java']
+lang2 = ['Python', 'Go', "C#"]
+
+langs = lang1 + lang2
+
+print("lang1: ", lang1)
+print("lang2: ", lang2)
+print("langs: ", langs)
