2f4ead19703685244b596d58efa9b503323c3ae5
Hannah Cho <hannahcho422@gmail.com>
2026-03-06 22:20:40 +0900

    init: Initial commit

---
+++ Basic/List/Practice05.py
@@ -0,0 +1,15 @@
+# Car Company
+
+car = ['Gene', 'Lex', 'Infini', 'Lambor', 'Linc']
+print("Current car models: ", car)
+
+car.append('Merce')
+print("Add Merce: ", car)
+
+car.remove('Lex')
+print("Lex discontinued: ", car)
+
+car.append('Gene-2019')
+print("Add Gene-2019: ", car)
+
+print("Developed second, third, fourth: ", car[1:4])
