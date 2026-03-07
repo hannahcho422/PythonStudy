4b43690c4fdcaaf12a7459a29ae2fbe3a1620ab9
Hannah Cho <hannahcho422@gmail.com>
2026-03-07 23:15:28 +0900

    init: Initial commit

---
+++ Basic/Function/Practice06.py
@@ -0,0 +1,23 @@
+# Draw a square where it's clicked
+
+import turtle
+
+def square(length):
+    for i in range(4):
+        t.forward(length)
+        t.left(90)
+
+def drawit(x, y):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    t.begin_fill()
+    t.color("green")
+    square(50)
+    t.end_fill()
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.onscreenclick(drawit)
+
+turtle.done()
